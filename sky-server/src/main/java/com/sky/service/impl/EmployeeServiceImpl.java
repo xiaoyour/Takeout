061c17010ae874a029a3d9defbe2792d07e7c234
xiaoyou <1721017386@qq.com>
2024-12-20 17:17:34 +0800

    新增 <添加员工> 功能

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
@@ -9,9 +11,12 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -53,6 +58,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    @Override
+    public void save(EmployeeDTO employeeDTO) {
+        Employee employee = new Employee();
+//      将从前端接收到的DTO对象属性赋给employee实体类
+        BeanUtils.copyProperties(employeeDTO, employee);
+//        设置状态为1
+        employee.setStatus(StatusConstant.ENABLE);
+//        设置默认密码为123456，并用MD5加密
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+//        设置创建时间
+        employee.setCreateTime(LocalDateTime.now());
+//        设置更新时间
+        employee.setUpdateTime(LocalDateTime.now());
+        //TODO:后期将下列两个属性修改为ID
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+//        调用DAO层将实体类写入数据库
+        employeeMapper.insert(employee);
+
     }
 
 }
