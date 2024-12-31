fab29d99aa223c3deb3a6c86765ac3f9532d79a1
xiaoyou <1721017386@qq.com>
2024-12-31 15:09:28 +0800

    增加功能 用户端历史订单模块：
    - 查询历史订单
    - 查询订单详情
    - 取消订单
    - 再来一单
    
    商家端订单管理模块：
    
    - 订单搜索
    - 各个状态的订单数量统计
    - 查询订单详情
    - 接单
    - 拒单
    - 取消订单
    - 派送订单
    - 完成订单

--- sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -3,6 +3,7 @@ package com.sky.mapper;
 
 import com.sky.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
@@ -12,4 +13,13 @@ import java.util.List;
 @Mapper
 public interface OrderDetailMapper {
     public void insertBatch(List<OrderDetail> shoppingCartList);
+
+
+    /**
+     * 根据订单id查询订单明细
+     * @param orderId
+     * @return
+     */
+    @Select("select * from order_detail where order_id = #{orderId}")
+    List<OrderDetail> getByOrderId(Long orderId);
 }
